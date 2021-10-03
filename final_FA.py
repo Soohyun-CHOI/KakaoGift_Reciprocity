3e4a65d7936af16ad378f52c86e936756dd166be
Soohyun <csh0sns@naver.com>
2021-10-03 18:16:13 +0900

    final 요인분석

--- final_FA.py
+++ final_FA.py
@@ -5,36 +5,60 @@ from statsmodels.stats.outliers_influence import variance_inflation_factor
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from factor_analyzer import FactorAnalyzer
 
 # 데이터 불러오기
 df = pd.read_excel("data/kakao_final.xlsx")
 
 # 데이터 전처리
-df_drop = df.drop(["No", "Q1_1", "Q1_2", "Q1_3", "Q1_4", "Q1_5", "Q2", "성별", "나이"], axis=1)  # 타임스탬프, 전화번호 칼럼 삭제
+df_drop = df.drop(["No", "Q1_1", "Q1_2", "Q1_3", "Q1_4", "Q1_5", "Q2", "성별", "나이"], axis=1)
 df_drop_colnames = df_drop.columns.tolist()  # 칼럼명 리스트 생성
 
 df_drop = df_drop.apply(pd.to_numeric, errors="coerce").fillna(0)  # 데이터 타입 숫자로 통일
 
 
-# 새로운 데이터프레임 생성
-"""
-칼럼 범주화 인덱스
-- 접근성: 0, 1
-- 네트워크성: 2, 3, 4 
-- 편의성: 5, 6, 7, 8 
-- 가시성: 9, 10, 11, 12
-- 표현성: 13
-- 기록성: 14
+# 탐색적 요인분석
 
-- 호혜성: 15, 16, 17, 18, 19, 20, 21, 22, 23
-- 인간관계: 24, 25, 26, 27, 28
-- 사용의도: 29
+# [1] x 값 DF 생성
+df_fa = df_drop.drop(["Q7_1", "Q7_2", "Q7_3", "Q7_4", "Q7_5", "Q7_6", "Q7_7", "Q7_8", "Q7_9", "Q8_1", "Q8_2", "Q8_3", "Q8_4", "Q8_5", "Q9_1"], axis=1)
+df_fa_colnames = df_fa.columns.tolist()
+
+# [2] 요인 수 선택
+fa = FactorAnalyzer(n_factors=15, rotation=None)
+fa.fit(df_fa)
+ev, v = fa.get_eigenvalues()
+
+plt.scatter(range(1, df_fa.shape[1]+1), ev)
+plt.plot(range(1, df_fa.shape[1]+1), ev)
+plt.title("Scree Plot", size=15)
+plt.xlabel("Factors")
+plt.ylabel("Eigenvalue")
+plt.grid()
+plt.show()  # Eigen 값이 1 이상일 때까지 or 그래프 기울기가 완만해지기 전까지 나누어야 데이터 결함 최소화
+
+# [3] 요인분석 실시
+fa_final = FactorAnalyzer(n_factors=3, rotation="varimax")  # ml: 최대우도 방법
+fa_final.fit(df_fa)
+efa_result = pd.DataFrame(fa_final.loadings_, index=df_fa_colnames)
+
+plt.figure(figsize=(5, 8))
+sns.heatmap(efa_result, cmap="Blues", annot=True, fmt=".2f")
+plt.title("Factor Analyze", size=15)
+plt.show()
+
+"""
+[요인분석 결과 index]
+A: 0, 1, 2, 3, 4, 5, 6, 7, 8
+B: 9, 10, 11, 12
+C: 13, 14
+** 0.5 이하 변수: 9, 12
 """
 
-df_drop["accessibility"] = np.mean(df_drop[df_drop_colnames[0:2]].T)  # 접근성
-df_drop["network"] = np.mean(df_drop[df_drop_colnames[2:5]].T)  # 네트워크성
-df_drop["convenience"] = np.mean(df_drop[df_drop_colnames[5:9]].T)  # 편의성
-df_drop["visibility"] = np.mean(df_drop[df_drop_colnames[9:13]].T)  # 가시성
+
+# FA 기준 새로운 데이터프레임 생성
+df_drop["A"] = np.mean(df_fa[df_fa_colnames[0:9]].T)
+df_drop["B"] = np.mean(df_fa[df_fa_colnames[9:13]].T)
+df_drop["C"] = np.mean(df_fa[df_fa_colnames[13:15]].T)
 
 df_drop["reciprocity"] = np.mean(df_drop[df_drop_colnames[15:24]].T)  # 호혜성
 df_drop["relationship"] = np.mean(df_drop[df_drop_colnames[24:29]].T)  # 인간관계
@@ -42,22 +66,14 @@ df_drop.rename(columns={df_drop_colnames[29]: "intention"}, inplace=True)  # 사
 
 df_drop_colnames_new = df_drop.columns.tolist()
 
-df_clean = df_drop[df_drop_colnames_new[-7:]]  # 범주별 평균 데이터프레임 생성
+df_clean = df_drop[df_drop_colnames_new[-6:]]  # 범주별 평균 데이터프레임 생성
 df_clean_colnames = df_clean.columns.tolist()
-
-
-# 상관계수 heatmap
-colormap = plt.cm.PuBu
-plt.figure(figsize=(10, 8))
-plt.title("Variable Correlation Heatmap", size=15)
-sns.heatmap(df_clean.corr(), vmax=1.0, square=True, cmap=colormap, annot=True)
-plt.show()
 
 
 # Multiple Linear Regression
 
 def fit_xy_model(y_name):
-    xy_model = ols(f"{y_name} ~ accessibility + network + convenience + visibility", df_clean)
+    xy_model = ols(f"{y_name} ~ A + B + C", df_clean)
     xy_res = xy_model.fit()
     y_name_ko = "호혜성" if y_name == "reciprocity" else "인간관계"
     print(f"< X 독립변수와 {y_name_ko} >")
